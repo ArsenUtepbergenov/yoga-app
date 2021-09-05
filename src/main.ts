ad786fa2df4e4d3cb83c79167c3add806f2e9081
Arsen Utepbergenov <arsen.utepbergenov@altenar.com>
2021-09-05 15:58:36 +0300

    master/changed bootstrap to ant

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { createApp } from "vue"
 // Application component
+import Antd from "ant-design-vue"
 import App from "./App.vue"
+import "ant-design-vue/dist/antd.css"
 // Vue global modules
 import { auth } from "@/firebase"
 import store, { storeKey } from "@/store"
 import router from "@/router"
-// The bootstrap
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
 // All global styles
 import "@/styles/base.scss"
 
@@ -17,6 +16,7 @@ auth.onAuthStateChanged(() => {
   if (!isInitApp) {
     createApp(App)
       .use(router)
+      .use(Antd)
       .use(store, storeKey)
       .mount("#app")
     isInitApp = true
